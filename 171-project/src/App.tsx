d8ac26a2f279f9df6133edb9adc16b6fa898b9fe
goldenfries100 <divyaraj940@gmail.com>
2023-11-22 12:00:04 -0800

    ImageInput (not finished)

--- 171-project/src/App.tsx
+++ 171-project/src/App.tsx
@@ -1,12 +1,14 @@
 import ListGroup from './components/Explanation';
 import Header from './components/Header';
 import Explanation from './components/Explanation';
+import ImageInput from './components/ImageInput';
 
 function App() {
   return (
     <>
       <div><Header /></div>
       <div><Explanation /></div>
+      <div><ImageInput /></div>
     </>
   );
 }
--- 171-project/src/components/ImageInput.tsx
+++ 171-project/src/components/ImageInput.tsx
@@ -1,6 +1,28 @@
+import React, { ChangeEvent, useState } from 'react';
 
-function ImageInput() {
-    //this will set up the place where the user inputs a file
-}
+const ImageInput: React.FC = () => {
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
+
+  const handleImageChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+
+    if (file && file.type.startsWith('image/')) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        const url = reader.result as string;
+        setImageUrl(url);
+      };
+
+      reader.readAsDataURL(file);
+    }
+  };
+
+  return (
+    <div>
+      <input type="file" accept="image/*" onChange={handleImageChange} />
+      {imageUrl && <img src={imageUrl} alt="Selected Image" style={{ maxWidth: '100%' }} />}
+    </div>
+  );
+};
 
 export default ImageInput;
