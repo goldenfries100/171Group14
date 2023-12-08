fe0b2e772685bd6206f8f83634bb7dd43ed329f6
goldenfries100 <divyaraj940@gmail.com>
2023-12-08 12:38:46 -0800

    Update Explanation.tsx

--- website-code/src/components/Explanation.tsx
+++ website-code/src/components/Explanation.tsx
@@ -3,7 +3,7 @@ function Explanation() {
     return (
     <>
         <h2>Group 14: A. Agarwal, S.Chiang, D. Raj, B. Young, H. Zhu</h2>
-        <h3>This is our website implementing the RNN-CNN model, which performed the best out of four models (CNN, KNN, RNN-CNN, Naive Bayes) that we developed during our testing/experimentation phase. Please input an image to test our model!</h3>
+        <h3>This is our website implementing the RNN-CNN model, which performed the best out of four models (CNN, KNN, RNN-CNN, Naive Bayes) that we developed during our testing/experimentation phase. Please input a 32x32 pixel image to test our model!</h3>
     </>
     );
 }
